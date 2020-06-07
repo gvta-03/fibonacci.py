2237c219f9c1c20e236a327a78a332ef4325fa90
gvta-03 <66378986+gvta-03@users.noreply.github.com>
2020-06-07 10:22:44 +0530

    codesnippets.py

---
+++ codesnippets.py
@@ -0,0 +1,8 @@
+#assigning elements to list
+list=[1,2,3,4]
+#accessing elements from tuple
+tup=("c","c++","python")
+print(tup)
+#deleting diff dictionary elements
+dic=("c","c++","python")
+dic.remove("C++")
